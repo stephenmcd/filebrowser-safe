2cb741b6e8ce2fe212bd5005bf455868f09dced0
Stephen McDonald <steve@jupo.org>
2016-01-17 11:39:48 +1100

    Version bump to 0.4.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="filebrowser_safe",
-    version="0.4.0",
+    version="0.4.1",
     description="A snapshot of the filebrowser_3 branch of django-filebrowser, "
                 "packaged as a dependency for the Mezzanine CMS for Django.",
     long_description=open("README.rst").read(),
